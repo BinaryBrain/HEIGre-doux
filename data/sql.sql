e19d813393632471e40b2aaa8ece58275232a1f2
D34D10K <leonard.berney@epfl.ch>
2015-01-18 22:41:59 +0100

    findFood now returns indexes

--- data/sql.sql
+++ data/sql.sql
@@ -8,6 +8,7 @@ declare word varchar(255) default "";
 
 drop temporary table if exists tmp;
 create temporary table tmp(
+id integer,
 name varchar(255)
 );
 
@@ -21,7 +22,7 @@ set word = concat(substring(word, 1 , char_length(word) - 1), "*");
 end if;
 
 if char_length(word) > 3 then
-insert into tmp select name from aliments where match (name) against (word in boolean mode) limit 1;
+insert into tmp select id, name from aliments where match (name) against (word in boolean mode) limit 1;
 end if;
 
 end while;
