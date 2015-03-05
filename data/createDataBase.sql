e2f242e10621c3c761a23ca8c36af4fa99dbad84
D34D10K <leonard.berney@epfl.ch>
2015-03-05 15:43:36 +0100

    create datatbase fix

--- data/createDataBase.sql
+++ data/createDataBase.sql
@@ -1,7 +1,7 @@
 create table `aliments` (`id` INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,`name` VARCHAR(254) NOT NULL,`occurrence` INTEGER DEFAULT 0 NOT NULL,`last` TIMESTAMP NOT NULL);
 create unique index `name_index` on `aliments` (`name`);
-create table `menus_aliments` (`id` INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,`id_menu` INTEGER NOT NULL,`id_aliment` INTEGER NOT NULL,`name` VARCHAR(254) NOT NULL,`type` INTEGER NOT NULL,`nutriment` INTEGER);
-create table `menus` (`id` INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,`date` TIMESTAMP NOT NULL);
+create table `menus_aliments` (`id` INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,`id_menu` INTEGER NOT NULL,`id_aliment` INTEGER,`name` VARCHAR(254) NOT NULL,`type` INTEGER NOT NULL,`nutriment` INTEGER);
+create table `menus` (`id` INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,`date` TIMESTAMP NOT NULL,`upvote` INTEGER NOT NULL,`downvote` INTEGER NOT NULL);
 create table `nutriments_aliments` (`id` INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,`name_F` VARCHAR(254) NOT NULL);
 create table `nutriments_names` (`id` INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,`name` VARCHAR(254) NOT NULL);
 create table `nutriments_values` (`id_nutriments_aliment` INTEGER NOT NULL,`id_nutriments_name` INTEGER NOT NULL,`value` DOUBLE NOT NULL,`unit` VARCHAR(254) NOT NULL,`matrix_unit` VARCHAR(254) NOT NULL,`value_type` VARCHAR(254) NOT NULL);
@@ -57,3 +57,8 @@ begin
 	where id = new.id_aliment;
 end;^^
 delimiter ;
+
+create view mostUpvoted as 
+select id, upvote
+from menus 
+order by upvote desc;
